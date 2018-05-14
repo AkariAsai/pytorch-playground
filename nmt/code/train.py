89d961090a5693d9194267b0a50894ddcbbab26f
Akari Asai <akari.1311aquamarine4@gmail.com>
2018-05-14 23:59:49 +0900

    fix codes

--- nmt/code/train.py
+++ nmt/code/train.py
@@ -240,9 +240,9 @@ for epoch in range(maxEpoch):
             # and update
             if devGleu >= bestDevGleu:
                 f_squad_context = open(
-                    './squad_context_bleu_' + str(devGleu) + '_perp_' str(devPerp) + '.txt', 'w')
+                    './squad_context_bleu_' + str(devGleu) + '_perp_' + str(devPerp) + '.txt', 'w')
                 f_squad_question = open(
-                    './squad_question_bleu_' + str(devGleu) + '_perp_' str(devPerp) + '.txt', , 'w')
+                    './squad_question_bleu_' + str(devGleu) + '_perp_' + str(devPerp) + '.txt', , 'w')
                 attention_file_path = './squad_attention_bleu_' + str(devGleu) + '_perp_' str(devPerp) + '.txt',
                 attention_scores_matrix = []
 
